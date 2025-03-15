2d7640bce3e420e2f8c0f66f759cebcd1b134b85
Sai Kiran <saikirankottur7@gmail.com>
2025-03-15 19:03:39 +0530

    Final Frontend Deployment

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   
     const claimCoupon = async () => {
       try {
-        const res = await fetch("https://coupon-claim-backend.onrender.com", {
+        const res = await fetch("https://coupon-claim-backend.onrender.com/claim-coupon", {
           method: "POST",
           credentials: "include",
           body: JSON.stringify({ guestID })
